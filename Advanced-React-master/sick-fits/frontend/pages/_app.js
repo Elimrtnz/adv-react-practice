e1e6cff06c2857461b9eafc64a5dc8abe8517373
Eli <>
2021-03-07 12:13:08 -0800

    add nprogress package while page routing

--- Advanced-React-master/sick-fits/frontend/pages/_app.js
+++ Advanced-React-master/sick-fits/frontend/pages/_app.js
@@ -1,9 +1,16 @@
 import Page from "../components/Page";
+import NProgress from "nprogress";
+import Router from "next/router";
+
+// adds loading progress bar to top of page
+// import "nprogress/nprogress.css"; #default
+import "../components/styles/nprogress.css";
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 // special file to control components higher than page
 // to access elements higher than the body
 export default function MyApp({ Component, pageProps }) {
-    return (
-        <Component {...pageProps} />
-    );
+  return <Component {...pageProps} />;
 }
